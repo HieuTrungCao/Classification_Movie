3ece393ba19aafa9faababfde334029275850830
HieuTrungCao <21021490@vnu.edu.vn>
2023-12-15 16:35:53 +0700

    Fix model

--- models/retnet.py
+++ models/retnet.py
@@ -61,7 +61,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.cls = nn.Linear(512 * block.expansion, hidden_state_img)
+        self.cls = nn.Linear(512 * block.expansion * block.expansion, hidden_state_img)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
