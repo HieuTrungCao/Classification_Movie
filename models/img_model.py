f37cddadea7b996054bc971e7ae1ebc976ee0acb
HieuTrungCao <21021490@vnu.edu.vn>
2023-12-19 09:36:58 +0700

    Change model image

---
+++ models/img_model.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+class ImgModel(nn.Module):
+    def __init__(self, hidden_state_img):
+        super(ImgModel, self).__init__()
+        self.hidden_state_img = hidden_state_img
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=1)
+        self.batch1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=7, stride=2, padding=3)
+        self.avg = nn.AvgPool2d(3, stride=1)
+        self.fc = nn.Linear(128 * 59 * 59, self.hidden_state_img)
+
+    def forward(self, img):
+        img = self.conv1(img)
+        img = self.batch1(img)
+        img = self.relu(img)
+        img = self.maxpool(img)
+        img = self.conv2(img)
+        img = self.avg(img)
+        img = img.view(img.size(0), -1)
+        img = self.fc(img)
+
+        return img
+
+    
--- models/model.py
+++ models/model.py
@@ -3,29 +3,30 @@ import torch
 
 from .lstm import LSTM
 from .resnet import resnet50
-
+from .img_model import ImgModel
 
 class Model(nn.Module):
     def __init__(self, num_class, hidden_state_img = 512, use_title=False, hidden_state_title = None, pretrained=True):
         super(Model, self).__init__()
 
-        self.input_dim = hidden_state_img
-
         self.use_title = use_title
-
         if use_title:
-            self.lstm = LSTM()
+            self.img_model = ImgModel(hidden_state_img)
+            self.input_dim = hidden_state_img
+            self.text_model = LSTM()
             self.input_dim += hidden_state_title
-        self.img_model = resnet50(hidden_state_img, pretrained=pretrained)
+            self.linear = nn.Linear(self.input_dim, num_class)
+        else:
+            self.img_model = ImgModel(num_class)
         
-        self.linear = nn.Linear(self.input_dim, num_class)
-
     def forward(self, img, title):
         
+        if not self.use_title:
+            out = self.img_model(img)
+            return out        
+
         out = self.img_model(img)
-        if self.use_title:
-            x = self.lstm(title)
-            out = torch.concat([out, x], dim=1)
-        out = self.linear(out)
+        t = self.text_model(title)
+        out = torch.concatenate((out, t), dim = 1)
 
         return out
