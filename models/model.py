3df07ce13740cdfaac9f8d2042381b87182cb956
HieuTrungCao <21021490@vnu.edu.vn>
2023-12-21 23:30:26 +0700

    Remove title

--- models/model.py
+++ models/model.py
@@ -15,19 +15,19 @@ class Model(nn.Module):
     self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
     self.fc_lstm = nn.Linear(hidden_dim, 128)
 
-    self.fc1 = nn.Linear(1128, 64)
-    self.fc2 = nn.Linear(64, num_classes)
+    # self.fc1 = nn.Linear(1128, 64)
+    self.fc2 = nn.Linear(1000, num_classes)
 
     self.dropout = nn.Dropout(0.2)
 
   def forward(self, image_tensor, title_tensor):
     cnn = self.img_model(image_tensor)
 
-    lstm = self.embed(title_tensor)
-    lstm, (hidden, cell) = self.lstm(lstm)
-    lstm_out = self.fc_lstm(hidden[-1])
+    # lstm = self.embed(title_tensor)
+    # lstm, (hidden, cell) = self.lstm(lstm)
+    # lstm_out = self.fc_lstm(hidden[-1])
 
-    out = F.relu(self.fc1(torch.concat([cnn, lstm_out], dim=1)))
-    out = self.fc2(out)
+    # out = F.relu(self.fc1(torch.concat([cnn, lstm_out], dim=1)))
+    out = self.fc2(cnn)
 
     return out
