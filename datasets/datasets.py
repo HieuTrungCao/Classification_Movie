2ea25a3b8105b32c3d0bcbc115176dbee10d7a3d
HieuTrungCao <21021490@vnu.edu.vn>
2023-12-22 22:01:40 +0700

    Fix dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -60,7 +60,6 @@ class MultiDataset(Dataset):
 
         # preprocess img
         img = cv2.imread(img_path)
-        self.count += 1
              
         img = cv2.resize(img, (224, 224))
         img_tensor = torch.from_numpy(img.transpose(2,0,1)).float()
