4467f46707767d6b2a64e173447555d50ae37945
HieuTrungCao <21021490@vnu.edu.vn>
2023-12-23 16:13:23 +0700

    fix get data

--- script/train.sh
+++ script/train.sh
@@ -1,1 +1,3 @@
-python train.py --path_data './data/dataset'
+python train.py --path_data './data/dataset' --batch_size 256 --save_path "/content/save_model" \
+  --threshold 0.6 --iter_print 4 --epoch 40 --pretrained True  --max_length 30 --use_title True\
+  --notes "Resnet 50 + distilBert + data aug" --nlp_model "distilbert-base-uncased" --get_year True
